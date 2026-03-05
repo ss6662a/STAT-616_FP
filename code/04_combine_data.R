a46af4ac2536431f57f94be906b872668ec1205d
Soren Snyder <ss6662a@american.edu>
2026-03-05 16:06:28 -0500

    added .geojson file to gitignore and started to combine data and deal with NAs

---
+++ code/04_combine_data.R
@@ -0,0 +1,34 @@
+
+
+
+# load data ----
+
+
+bike <- read_csv(here("data", "daily_rides.csv"))
+weather <- read_csv(here("data", "weather_clean.csv"))
+income <- st_read(here("data", "dc_income.geojson"))
+
+
+
+# dealing with missing values ----
+
+colSums(is.na(bike)) # 0 without ebikes
+colSums(is.na(weather)) # need to deal with
+colSums(is.na(income)) # 5 missing estimates for median income, probably okay to drop
+
+## weather NAs ----
+
+# probably should just take station or 
+# want to deal with by averaging temp for that week
+# I don't know if we have the data for that tough and the small area of DC makes me lean toward using one good station instead
+
+
+
+
+
+
+
+
+
+
+
