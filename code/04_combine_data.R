39ab68dc71a5a48bedbe88b728e800f7b78b442e
Soren Snyder <ss6662a@american.edu>
2026-03-06 11:13:04 -0500

    joined bikeshare, weather, and income data. however, will probably need to get income data from DC suburbs in MD and VA to prevent a lot of missing data

--- code/04_combine_data.R
+++ code/04_combine_data.R
@@ -5,28 +5,43 @@
 
 
 bike <- read_csv(here("data", "daily_rides.csv"))
-weather <- read_csv(here("data", "weather_clean.csv"))
+weather <- read_csv(here("data", "dca_weather.csv"))
 income <- st_read(here("data", "dc_income.geojson"))
 
+head(bike)
+head(weather)
+head(income)
 
 
 # dealing with missing values ----
 
 colSums(is.na(bike)) # 0 without ebikes
-colSums(is.na(dca_weather)) # much better
+colSums(is.na(weather)) # much better after switching to dca station
 colSums(is.na(income)) # 5 missing estimates for median income, probably okay to drop
 
-## weather NAs ----
 
-# probably should just take station or 
-# want to deal with by averaging temp for that week
-# I don't know if we have the data for that tough and the small area of DC makes me lean toward using one good station instead
+# joining data ----
+
+# create an sf object from bike lat long data
+bike_stations_sf <- bike %>% 
+  distinct(start_station_id, start_station_name, start_lat, start_lng) %>% 
+  st_as_sf(coords = c("start_lng", "start_lat"), crs = 4326)
+
+bike_stations_income <- st_join(bike_stations_sf, income) %>% 
+  st_drop_geometry() %>% 
+  select(start_station_id, GEOID, est_med_income, est_pct_poverty, est_total_pop) %>% 
+  group_by(start_station_id) %>% # points on boarders duplicate, 26 times, so just keeping first instance
+  slice(1) %>%
+  ungroup()
 
 
+full_data <- bike %>% 
+  left_join(bike_stations_income, by = "start_station_id") %>% 
+  left_join(weather %>%  select(-station), by = "date")
+ 
+# need to deal with missing values still
 
-
-
-
+colSums(full_data)
 
 
 
