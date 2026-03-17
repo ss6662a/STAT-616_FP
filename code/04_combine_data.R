3ddd3a70b25ed8faf3f7e7bc7e40cc7e22a3ac46
Soren Snyder <ss6662a@american.edu>
2026-03-16 21:18:07 -0400

    dealt with missing values; non-2025 dates and non-dc stations. imputed awnd NAs with weekly average

--- code/04_combine_data.R
+++ code/04_combine_data.R
@@ -16,7 +16,7 @@ head(income)
 # dealing with missing values ----
 
 colSums(is.na(bike)) # 0 without ebikes
-colSums(is.na(weather)) # much better after switching to dca station
+colSums(is.na(weather)) # much better after switching to dca station (6 from awnd)
 colSums(is.na(income)) # 5 missing estimates for median income, probably okay to drop
 
 
@@ -32,18 +32,33 @@ bike_stations_income <- st_join(bike_stations_sf, income) %>%
   select(start_station_id, GEOID, est_med_income, est_pct_poverty, est_total_pop) %>% 
   group_by(start_station_id) %>% # points on boarders duplicate, 26 times, so just keeping first instance
   slice(1) %>%
-  ungroup()
+  ungroup() %>% 
+  filter(!is.na(est_med_income)) # capital bikeshare extends outside of DC proper, and we didn't get that income data, so we're removing them
 
 
-full_data <- bike %>% 
+full_data <- bike %>%
+  filter(
+    start_station_id %in% bike_stations_income$start_station_id # removes non-dc stations
+    ) %>%
+  filter(!date == "2024-12-31") %>% # contains one day in 2024 I want to remove
   left_join(bike_stations_income, by = "start_station_id") %>% 
   left_join(weather %>%  select(-station), by = "date")
- 
+
 # need to deal with missing values still
 
 colSums(is.na(full_data))
+# only missing data is in wind speed. Will replace missing values with average weekly wind speed for the NA's week
 
+ready_data <- full_data %>% 
+  mutate(week_t = week(date)) %>% # temporary week variable
+  group_by(week_t) %>% 
+  mutate(
+    awnd = if_else(is.na(awnd),
+                   mean(awnd, na.rm = TRUE),
+                   awnd)
+  ) %>% 
+  ungroup() %>% 
+  select(-week_t)
 
-
-
+colSums(is.na(ready_data))
 
